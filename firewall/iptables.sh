f66b01e1138cb28c3ee0366f60808f0575c119ab
elnataoliveira <elnataoliveira@gmail.com>
2020-05-27 16:55:20 -0300

    update iptables

--- firewall/iptables.sh
+++ firewall/iptables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ifconfig 
-sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo su -
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i <interface> -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 #iptables -I DOCKER-USER -i <interface> ! -s <ip>/24 -j ACCEPT

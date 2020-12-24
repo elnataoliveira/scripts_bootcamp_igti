ff1c61950c432d55e54abb1f2103bf89928ec0b4
Elnatã Oliveira <elnataoliveira@gmail.com>
2020-12-24 11:41:59 -0300

    Update docker.sh

--- container/docker.sh
+++ container/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker pull wordpress
 docker pull phpmyadmin/phpmyadmin
-docker run --name phpmyadmin -d -e PMA_HOST=ip_host -p 8080:80 phpmyadmin/phpmyadmin
-docker run --name wordpress -e WORDPRESS_DB_HOST=ip_host:3306 -p 8100:80 -e WORDPRESS_DB_USER=user -e WORDPRESS_DB_PASSWORD=password -d wordpress
+docker run  --restart=always --name phpmyadmin -d -e PMA_HOST=ip_host -p 8080:80 phpmyadmin/phpmyadmin
+docker run  --restart=always --name wordpress -e WORDPRESS_DB_HOST=ip_host:3306 -p 8100:80 -e WORDPRESS_DB_USER=user -e WORDPRESS_DB_PASSWORD=password -d wordpress
 docker ps 
+

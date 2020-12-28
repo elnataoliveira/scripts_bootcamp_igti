33ffbf33b9d85f0c690d01cfa501ace45597ef82
Elnatã Oliveira <elnataoliveira@gmail.com>
2020-12-28 10:51:24 -0300

    Update docker.sh

--- container/docker.sh
+++ container/docker.sh
@@ -5,3 +5,21 @@ docker run  --restart=always --name phpmyadmin -d -e PMA_HOST=ip_host -p 8080:80
 docker run  --restart=always --name wordpress -e WORDPRESS_DB_HOST=ip_host:3306 -p 8100:80 -e WORDPRESS_DB_USER=user -e WORDPRESS_DB_PASSWORD=password -d wordpress
 docker ps 
 
+
+
+Proxy
+
+mkdir /etc/systemd/system/docker.service.d
+[Service]
+Environment="HTTP_PROXY=http://proxy.example.com:80/"
+Environment="NO_PROXY=localhost,127.0.0.0/8,docker-registry.somecorporation.com"
+
+$ sudo systemctl daemon-reload
+
+$ sudo systemctl show --property Environment docker
+Environment=HTTP_PROXY=http://proxy.example.com:80/
+
+$ sudo systemctl restart docker
+
+
+

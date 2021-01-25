cd7bf0d759c8ea91a14d51a311416ae5c61c31c5
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-01-24 22:37:07 -0300

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -2,6 +2,7 @@
 
 wget https://repo.zabbix.com/zabbix/5.2/debian/pool/main/z/zabbix-release/zabbix-release_5.2-1+debian10_all.deb
 dpkg -i zabbix-release*.deb
+apt update
 apt install zabbix-server-mysql
 apt install zabbix-frontend-php
 sudo apt install zabbix-apache-conf

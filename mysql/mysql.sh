1a48734daeea928da05bc12f6fdda07bb8313c2d
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-01-26 14:05:42 -0300

    Update mysql.sh

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -4,10 +4,8 @@ apt -y upgrade && update
 apt clean
 apt install default-mysql-server
 mysql -q -e "source $(pwd)/script.sql"
-apt install php php-mysql
-vi /etc/mysql/mysql.conf.d/mysqld.cnf
-#edite bind-address para 0.0.0.0 
-/etc/init.d/mysql restart
+
+mysql_upgrade --user=root --password=pass**** --force
 #mysql tunner
 apt insssall mysqltuner
 mysqltuner

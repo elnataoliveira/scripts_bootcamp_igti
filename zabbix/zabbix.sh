ebcca486d9dbe45b647f27c5d484dcdd8a119322
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-01-25 21:39:59 -0300

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 wget https://repo.zabbix.com/zabbix/5.2/debian/pool/main/z/zabbix-release/zabbix-release_5.2-1+debian10_all.deb
+https://repo.zabbix.com/zabbix/5.3/debian/pool/main/z/zabbix-release/zabbix-release_5.3-1%2Bdebian9_all.deb
 dpkg -i zabbix-release*.deb
 apt update
 apt install zabbix-server-mysql
@@ -9,6 +10,9 @@ sudo apt install zabbix-apache-conf
 sudo apt install zabbix-agent
 
 references: https://www.zabbix.com/download
+
+https://wiki.gentoo.org/wiki/Apache/pt-br
+
 
 
 

88977ee9ce97982cca25784f0e380db17cf61744
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-01-28 15:24:10 -0300

    Update docker.sh

--- container/docker.sh
+++ container/docker.sh
@@ -4,8 +4,10 @@ docker pull phpmyadmin
 docker pull mariadb/server
 
 docker run  --restart=always --name wordpress -e WORDPRESS_DB_HOST=ip_host:3306 -p 8100:80 -e WORDPRESS_DB_USER=user -e WORDPRESS_DB_PASSWORD=password -d wordpress
-docker run --name --restart=always -d --link application-server:db --p 80:80 -p 443:443 -p 22:22 phpmyadmin/phpmyadmin
-docker run  --restart=always --name database-server -d -e MARIADB_ROOT_PASSWORD=passwd -p 3306:3306 -p 26:22 mariadb/server
+docker run --name --restart=always -d --link application-server:database --p 80:80 -p 443:443 -p 22:22 phpmyadmin/phpmyadmin
+docker run  --restart=always --name database-server -d -e MARIADB_ROOT_PASSWORD=passwd -p 3306:3306 -p 26:22 -v /var/lib/mysql:/var/lib/mysql mariadb/server
+
+docker run -d --name meucontainer -v /var/app imagem
 
 docker ps 
 

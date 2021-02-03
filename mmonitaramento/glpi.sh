71a5f53cfd5dc70846cbe14a66782acaf75f6bdf
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-02-03 11:57:25 -0300

    Update and rename glpi.md to glpi.sh

--- mmonitaramento/glpi.sh
+++ mmonitaramento/glpi.sh
@@ -1,4 +1,3 @@
-```sh
 wget https://github.com/glpi-project/glpi/releases/download/9.5.3/glpi-9.5.3.tgz
 sudo tar xvf ~/glpi-9.5.3.tgz
 mv ~/glpi/ /var/www/html/
@@ -6,4 +5,3 @@ chown -R www-data:www-data glpi/
 apt install php-cas php-intl php-apcu  php-xmlrpc php-ldap
 service apache2 restart
 rm -rf install.php
-```

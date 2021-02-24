72810934238bdc02f73b12ddd4cfac8aeee1d75c
Elnatã Oliveira <elnataoliveira@gmail.com>
2021-02-24 18:56:27 -0300

    Create install.sh

---
+++ ansible/install.sh
@@ -0,0 +1,12 @@
+### Ansible in host windows on WinRM
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+### Install Ansible on ubuntu
+
+apt install ansible
